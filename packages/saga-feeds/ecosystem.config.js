ffb1d6fb75e2b4682318443a309cc4439c2f744f
James Cole <mail@ofjamescole.com>
2020-02-12 11:03:34 -0500

    add pm2 config file

---
+++ packages/saga-feeds/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'saga-api',
+      script: './index.js',
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'saga-feed-updater',
+      script: './daemon/feed-updater.js',
+      watch: true,
+      autorestart: true,
+      args: '-d',
+    },
+  ],
+}
