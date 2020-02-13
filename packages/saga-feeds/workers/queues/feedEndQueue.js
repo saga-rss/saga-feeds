3f8586cf332dd57de58997f637fe6500646e6567
James Cole <mail@ofjamescole.com>
2020-02-13 17:48:42 -0500

    remove separate queue types file

--- packages/saga-feeds/workers/queues/feedEndQueue.js
+++ packages/saga-feeds/workers/queues/feedEndQueue.js
@@ -1,17 +1,19 @@
-const Queue = require('bull')
+const Queue = require("bull");
 
-const types = require('../types')
-const processor = require('../processors/feedEndProcess')
-const PROCESS_LIMIT = 20
+const processor = require("../processors/feedEndProcess");
+const PROCESS_LIMIT = 20;
+const RSS_NEW_FEED_END = "rss/NEW_FEED_END";
 
-const FeedEndQueue = new Queue(types.RSS_NEW_FEED_END)
-const FeedEndQueueAdd = FeedEndQueue.add.bind(FeedEndQueue)
-const FeedEndQueueProcess = () => FeedEndQueue.process(PROCESS_LIMIT, processor)
-const FeedEndQueueStop = () => FeedEndQueue.close()
+const FeedEndQueue = new Queue(RSS_NEW_FEED_END);
+const FeedEndQueueAdd = FeedEndQueue.add.bind(FeedEndQueue);
+const FeedEndQueueProcess = () =>
+  FeedEndQueue.process(PROCESS_LIMIT, processor);
+const FeedEndQueueStop = () => FeedEndQueue.close();
 
 module.exports = {
+  RSS_NEW_FEED_END,
   FeedEndQueue,
   FeedEndQueueAdd,
   FeedEndQueueProcess,
-  FeedEndQueueStop,
-}
+  FeedEndQueueStop
+};
--- packages/saga-feeds/workers/types.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  RSS_NEW_FEED_START: "rss/NEW_FEED_START",
-  RSS_NEW_FEED_END: "rss/NEW_FEED_END",
-  META_START: "meta/START",
-  META_END: "meta/END"
-};
